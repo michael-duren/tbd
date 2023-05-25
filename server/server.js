d9145c533063deeb0483d069256bd9614c54b63b
Michael Duren <michaeld@michaelduren.com>
2023-05-24 19:32:05 -0500

    update PORT variable

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const { getAllSongs, getSingleSong, createSong } = require('./db');
 
 const app = express();
-process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.listen(PORT, () => {
